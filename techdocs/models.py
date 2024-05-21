a615cbe76ca270fc77e3127375ea5ebc0b1f1f86
Owen <69588466+owensdyer@users.noreply.github.com>
2024-05-21 11:23:36 +0100

    Adding slug attributes and supercategory model

--- techdocs/models.py
+++ techdocs/models.py
@@ -2,27 +2,49 @@ from django.db import models
 
 
 # Create your models here.
+class Supercategory(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name = 'Supercategory'
+        verbose_name_plural = 'Supercategories'
+
+    def __str__(self):
+        return self.name
+
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='child_category', on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
 
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-
+        ordering = ['name']
+        unique_together = ('parent', 'slug')
 
     def __str__(self):
-        return self.name
+        if self.parent is None:
+            return str(self.name)
+        else:
+            return str(self.parent) + " / " + self.name
 
 
 class Subcategory(models.Model):
     name = models.CharField(max_length=20, verbose_name='Subcategories'),
     description = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    parent = models.ForeignKey('Category', null=True, blank=True, related_name='child_subcategory', on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
 
     class Meta:
         verbose_name = 'Subcategory'
         verbose_name_plural = 'Subcategories'
+        unique_together = ('parent', 'slug')
 
     def __str__(self):
         return self.name
@@ -32,10 +54,12 @@ class TechnicalDoc(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     category = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
 
     class Meta:
         verbose_name = 'Technical Document'
         verbose_name_plural = 'Technical Documents'
+        unique_together = ('category', 'slug')
 
     def __str__(self):
         return str(self.category) + "/" + self.title
