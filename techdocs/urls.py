50e461d5139a59dea87dd31e84f850500914a099
Owen <69588466+owensdyer@users.noreply.github.com>
2024-05-21 11:22:42 +0100

    Added category and doc urls

--- techdocs/urls.py
+++ techdocs/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # category urls
+    path('category/create/', views.create_category, name='create_category'),
+    path('category/update/<int:pk>/', views.update_category, name='edit_category'),
+    path('category/delete/<int:pk>/', views.delete_category, name='delete_category'),
+
+    # technical documentation
+    path('document/create/', views.create_category, name='create_category'),
+    path('document/update/<slug:pk>/', views.update_category, name='edit_category'),
+    path('document/delete/<slug:pk>/', views.delete_category, name='delete_category'),
 ]
