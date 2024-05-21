9093b428a6a564f47eafa48ed166cebe3054df8e
Owen <69588466+owensdyer@users.noreply.github.com>
2024-05-21 15:26:34 +0100

    Working on nested slugs

--- techdocs/views.py
+++ techdocs/views.py
@@ -5,8 +5,24 @@ from . import models
 
 # Create your views here.
 def index(request):
+    obj = models.Supercategory.objects.all()
+
+    return render(request, 'techdocs/index.html', {'supercategories': obj})
+
+
+def view_category(request, category):
+    # objt = models.Category.objects.get(slug=category)
+    # print(objt.parents.all())
+
     obj = models.Category.objects.all()
-    return render(request, 'techdocs/index.html', {'categories': obj})
+
+    return render(request, 'techdocs/view_category.html', {'categories': obj})
+
+
+def view_document(request, category, document):
+    obj = models.TechnicalDoc.objects.all()
+    print(obj)
+    return render(request, 'techdocs/view_document.html', {'doc': obj})
 
 
 def create_category(request):
