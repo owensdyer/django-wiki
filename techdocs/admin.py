1dcdf8c9c742dce135a9804de4b0a09a7f746aea
Owen <69588466+owensdyer@users.noreply.github.com>
2024-05-21 15:24:56 +0100

    Adding ModelAdmin classes for models

--- techdocs/admin.py
+++ techdocs/admin.py
@@ -2,14 +2,24 @@ from django.contrib import admin
 
 from . import models
 
+
 # Register your models here.
-admin.site.register(models.Supercategory)
-admin.site.register(models.Category)
-admin.site.register(models.Subcategory)
-admin.site.register(models.TechnicalDoc)
-
-
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ["name"]}
     list_display = ["parent", "name"]
-    
+
+
+class SubcategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["name"]}
+    list_display = ["parent", "name", "description"]
+
+
+class TechnicalDocAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["title"]}
+    list_display = ["category", "title"]
+
+
+admin.site.register(models.Supercategory)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Subcategory, SubcategoryAdmin)
+admin.site.register(models.TechnicalDoc, TechnicalDocAdmin)
